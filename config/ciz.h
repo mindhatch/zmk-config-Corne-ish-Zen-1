54a38092df3303048de5da0d2158b35b470a6fe2
mindhatch <mtwhrpr@gmail.com>
2021-12-24 19:58:04 -0500

    oops i did it again

--- config/ciz.h
+++ config/ciz.h
@@ -1,5 +1,6 @@
 #define SK(KEY) &nk KEY KEY
 #define MO(LAYER) &mo_tap LAYER F24
+#define MO(LAYER) &mo_tap LAYER key_repeat
 
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
